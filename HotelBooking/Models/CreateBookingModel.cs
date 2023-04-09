28e49f943e3735978aed313c667869f352595ddc
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2023-04-09 10:48:52 -0500

    Create booking model

---
+++ HotelBooking/Models/CreateBookingModel.cs
@@ -0,0 +1,25 @@
+﻿namespace HotelBooking.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateBookingModel
+    {
+        public CreateBookingModel()
+        {
+            GuestEmail = string.Empty;
+        }
+
+        [Required]
+        public byte RoomId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string GuestEmail { get; set; }
+
+        [Required]
+        public DateTime ReservationStartDateUtc { get; set; }
+
+        [Required]
+        public DateTime ReservationEndDateUtc { get; set; }
+    }
+}
