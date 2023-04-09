39f5dbcb1307aa56dcae59c8a88d1b52653388bf
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2023-04-09 04:03:10 -0500

    Make connections async.

---
+++ HotelBooking.Persistence/SqlServer/ISqlConnection.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelBooking.Persistence.SqlServer
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public interface ISqlConnection : IDisposable
+    {
+        Task OpenAsync();
+
+        Task CloseAsync();
+
+        SqlCommand CreateCommand();
+    }
+}
---
+++ HotelBooking.Persistence/SqlServer/SqlConnectionWrapper.cs
@@ -0,0 +1,34 @@
+﻿namespace HotelBooking.Persistence.SqlServer
+{
+    using System.Data.SqlClient;
+
+    public class SqlConnectionWrapper : ISqlConnection
+    {
+        private readonly SqlConnection _connection;
+
+        public SqlConnectionWrapper(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+
+        public async Task OpenAsync()
+        {
+            await _connection.OpenAsync().ConfigureAwait(false);
+        }
+
+        public async Task CloseAsync()
+        {
+            await _connection.CloseAsync().ConfigureAwait(false);
+        }
+
+        public SqlCommand CreateCommand()
+        {
+            return _connection.CreateCommand();
+        }
+    }
+}
