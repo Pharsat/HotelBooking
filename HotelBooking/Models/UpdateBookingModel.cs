97a5486874d72c949aeb434dc76a29c1f676490f
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2023-04-09 10:53:18 -0500

    Add update booking model

---
+++ HotelBooking/Models/UpdateBookingModel.cs
@@ -0,0 +1,28 @@
+﻿namespace HotelBooking.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class UpdateBookingModel
+    {
+        public UpdateBookingModel()
+        {
+            GuestEmail = string.Empty;
+        }
+
+        [Required]
+        public long BookingId { get; set; }
+
+        [Required]
+        public byte RoomId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string GuestEmail { get; set; }
+
+        [Required]
+        public DateTime ReservationStartDateUtc { get; set; }
+
+        [Required]
+        public DateTime ReservationEndDateUtc { get; set; }
+    }
+}
